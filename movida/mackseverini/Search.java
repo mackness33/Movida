ea10139cb6ad8aa31a9407459c0680eb92b67fc9
mackness33 <davidantonhy962@gmail.com>
2020-05-10 13:04:16 +0200

    🚧 BinarySearch workin
    Made BinarySearch to do a first check on Movies and Persons, still need to be implement though

--- movida/mackseverini/Search.java
+++ movida/mackseverini/Search.java
@@ -2,11 +2,8 @@ package movida.mackseverini;
 
 import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
 
 import movida.commons.Movie;
 import movida.commons.Person;
@@ -15,5 +12,39 @@ import java.lang.Integer;
 public class Search{
 
   //Search use to understand if the obj is in the list and where
-  public static <T> T binarySearch(List<T> T){}
+  public static /*<T extends Integer/*extends Comparable<T>*/ Integer binarySearch(List<Integer> map, Integer el){
+    // Object [] array = map.toArray();     //the array must be already sorted!
+    Integer [] array = new Integer [80];     //the array must be already sorted!
+    array = map.toArray(array);     //the array must be already sorted!
+    Integer first = 0, last = map.size() - 1, middle = last/2 + 1;
+
+    while (first <= last){
+      if (first == el)
+        return first;
+      else if (last == el)
+        return last;
+      else if (middle == el)
+        return middle;
+
+      System.out.println("first = " + first);
+      System.out.println("last = " + last);
+      System.out.println("middle = " + middle);
+
+      //`middle == first` == `first == last` so not needed
+      if (first == last || middle == last || first > last)
+        break;
+
+      System.out.println("checks: " + (array[middle] < el));
+      if (array[middle] > el){
+        last = middle - 1;
+        middle = middle - ((middle - first) / 2) + 1;
+      }
+      else{
+        first = middle + 1;
+        middle = middle + ((last - middle) / 2) + 1;
+      }
+    }
+
+    return new Integer(-1);
+  }
 }
--- test.java
+++ test.java
@@ -1,18 +1,35 @@
 import movida.mackseverini.MovidaDB;
+import movida.mackseverini.Search;
 import java.io.File;
 import java.util.LinkedList;
 import movida.commons.Movie;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class test {
 
   public static void main(String args[]){
     // LinkedList<Movie> M = new LinkedList<Movie>();
     // MovidaDB mb = new MovidaDB(M);
-    MovidaDB mb = new MovidaDB();
-    mb.init_class();
+    // MovidaDB mb = new MovidaDB();
+    // mb.init_class();
+    //
+    // mb.loadFromFile(new File("movida/assets/esempio-formato-dati.txt"));
+    //
+    // mb.printMovies();
 
-    mb.loadFromFile(new File("movida/assets/esempio-formato-dati.txt"));
+    int min = 1, max = 70;
+    LinkedList<Integer> M = new LinkedList<Integer>();
+    // nextInt is normally exclusive of the top value,
+    // so add 1 to make it inclusive
+    int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
 
-    mb.printMovies();
+    for (int i = min; i <= max; i++)
+      M.add(i);
+
+    Integer result = Search.binarySearch(M, randomNum);
+    
+    System.out.println("size = " + M.size());
+    System.out.println("random = " + randomNum);
+    System.out.println("result = " + result);
   }
 }
