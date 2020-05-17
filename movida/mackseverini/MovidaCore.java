21c7225a906aa135f41a14caf963bed576c5330a
mackness33 <davidantonhy962@gmail.com>
2020-05-17 19:53:16 +0200

    🎨 Add of MovidaCore
    First idea of MovidaCore

---
+++ movida/mackseverini/MovidaCore.java
@@ -0,0 +1,160 @@
+package movida.mackseverini;
+
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import movida.commons.Movie;
+import movida.commons.Person;
+import java.lang.Integer;
+import movida.mackseverini.Search;
+
+public class MovidaCore implements movida.commons.IMovidaDB {
+  private LinkedList<Movie> movies;
+  private LinkedList<Person> people;
+
+  public MovidaCore(){
+    this.movies = null;
+    this.people = null;
+  }
+
+  public MovidaCore(LinkedList<Movie> M, LinkedList<Person> P){
+    this.movies = M;
+    this.people = P;
+  }
+
+  public void init_class(){
+    this.movies = new LinkedList<Movie>();
+    this.people = new LinkedList<Person>();
+  }
+
+  public void printMovies(){
+    Iterator<Movie> iterator = this.movies.iterator();
+
+    // System.out.println("Film uploaded: " + this.movies.size());
+
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+      System.out.println();
+
+    }
+  }
+
+  public void printPeople(){
+    Iterator<Person> iterator = this.people.iterator();
+
+    // System.out.println("People added: " + this.people.size());
+
+    while (iterator.hasNext())
+      System.out.println(iterator.next());
+  }
+
+  @Override
+  public void loadFromFile(File f){
+
+    try{
+      BufferedReader br = new BufferedReader(new FileReader(f));
+      String [] movie = new String [5];
+      String [] line = new String [2];
+      String stream = "";
+
+      while ((stream = br.readLine()) != null){
+        System.out.println(stream);
+
+        line = stream.split(":");
+
+        // Using a switch so even though the movie is describe in a unordered way
+        //  it'll still be right
+        switch(line[0]){
+          case "Title": movie[0] = line[1].trim(); break;
+          case "Year": movie[1] = line[1].trim(); break;
+          case "Votes": movie[2] = line[1].trim(); break;
+          case "Cast": movie[3] = line[1].trim(); break;
+          case "Director": movie[4] = line[1].trim(); break;
+          case "": this.addMovie(movie); break;
+          default: System.out.println("Something went wrong!");
+        }
+      }
+
+      this.addMovie(movie);
+
+      br.close();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
+
+  private void addMovie(String [] movie){
+    Person [] people = new Person [10];
+    String [] useless = movie[3].split(",");
+    int pos = -1;
+    Movie temp = null;
+
+    this.addPerson(movie[4]);
+
+    for(int i = 0; i < 10; i++){
+      if (i < useless.length){
+        this.addPerson(useless[i].trim());
+        people[i] = new Person(useless[i].trim());
+      }
+      else
+        people[i] = null;
+    }
+
+    temp = new Movie(movie[0], new Integer(movie[1]), new Integer(movie[2]), people, new Person(movie[4]));
+    pos = Search.<Movie, Movie>dumbSearch(movies, temp);
+
+    if (pos < 0)
+      movies.add(temp);
+    else
+      movies.get(pos).update(temp);
+  }
+
+  private void addPerson(String name){
+    Person temp = null;
+    int pos = -1;
+    temp = new Person(name);
+    pos = Search.<Person, Person>dumbSearch(people, temp);
+
+    System.out.println("pos: " + pos);
+    if (pos < 0)
+      people.add(temp);
+    else
+      System.out.println("ALREADY THERE");
+
+
+    System.out.println("Add of person!");
+  }
+
+  @Override
+	public void saveToFile(File f){}
+
+	@Override
+	public void clear(){}
+
+	@Override
+	public int countMovies(){ return 0; }
+
+	@Override
+	public int countPeople(){ return 0; }
+
+	@Override
+	public boolean deleteMovieByTitle(String title){ return false; }
+
+  @Override
+	public Movie getMovieByTitle(String title){ return null; }
+
+  @Override
+	public Person getPersonByName(String name){ return null; }
+
+  @Override
+	public Movie[] getAllMovies(){ return null; }
+
+	@Override
+	public Person[] getAllPeople(){ return null; }
+}
--- test.java
+++ test.java
@@ -1,4 +1,5 @@
 import movida.mackseverini.MovidaDB;
+import movida.mackseverini.MovidaCore;
 import movida.mackseverini.Search;
 import java.io.File;
 import java.util.LinkedList;
@@ -8,31 +9,13 @@ import java.util.concurrent.ThreadLocalRandom;
 public class test {
 
   public static void main(String args[]){
-    MovidaDB mb = new MovidaDB();
+    // MovidaDB mb = new MovidaDB();
+    MovidaCore mb = new MovidaCore();
     mb.init_class();
 
     mb.loadFromFile(new File("movida/assets/esempio-formato-dati.txt"));
 
     mb.printMovies();
     mb.printPeople();
-
-    //test I use, can be cleaned!
-
-    // int min = 1, max = 70;
-    // LinkedList<Integer> M = new LinkedList<Integer>();
-    // // nextInt is normally exclusive of the top value,
-    // // so add 1 to make it inclusive
-    // int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
-    //
-    // for (int i = min; i <= max; i++)
-    //   M.add(i);
-    //
-    // Integer rst_binary = Search.binarySearch(M, randomNum);
-    // Integer rst_dumb = Search.dumbSearch(M, randomNum);
-    //
-    // System.out.println("size = " + M.size());
-    // System.out.println("random = " + randomNum);
-    // System.out.println("result binary = " + rst_binary);
-    // System.out.println("result dumb = " + rst_dumb);
   }
 }
