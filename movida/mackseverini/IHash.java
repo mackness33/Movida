19f3ba51c96a5b4a7a25ea67044782824b65ae37
mackness33 <davidantonhy962@gmail.com>
2020-09-16 16:06:45 +0200

    🔥 minor change

--- movida/mackseverini/IHash.java
+++ movida/mackseverini/IHash.java
@@ -1,7 +1,6 @@
 package movida.mackseverini;
 
 public interface IHash<T> extends movida.mackseverini.IMap<T> {
-  public final movida.commons.MapImplementation type = movida.commons.MapImplementation.HashConcatenamento;
 
   public void reset ();
 
