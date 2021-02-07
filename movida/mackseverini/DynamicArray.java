b5c2d2a8aef7107363d0f4d9b69df380c0fc9272
David Mack <mack.david.88@gmail.com>
2021-02-07 16:29:24 +0100

    :memo: Added comments to DynamicArray

--- movida/mackseverini/DynamicArray.java
+++ movida/mackseverini/DynamicArray.java
@@ -3,8 +3,8 @@ package movida.mackseverini;
 import movida.mackseverini.Queue;
 import java.util.Arrays;
 
-// Class used to virtually implements an array without its costraints
-public class DynamicArray<E extends Comparable<E>> {
+// Extension of Array that manage also the length of the array
+public class DynamicArray<E extends Comparable<E>> extends movida.mackseverini.Array<E>{
 
 	private final Object[] arr;
 	public final int length;
@@ -32,26 +32,13 @@ public class DynamicArray<E extends Comparable<E>> {
 		this.length = shallow.length;
 	}
 
-	// Function to get Object present at index i in the array
-	@SuppressWarnings("unchecked")
-	public E get(int i) {
-		final E e = (E)arr[i];
-		return e;
-	}
-
-	// Function to set a value e at index i in the array
-	@SuppressWarnings("unchecked")
-	public void set(int i, E e) {
-		arr[i] = e;
-	}
-
 	public DynamicArray trim() {
 		Queue<Integer> Q = new Queue();
-		
+
 		for(Integer i = 0; i < this.length; i++)
 			if (arr[i] != null)
 				Q.enqueue(i);
-		
+
 		DynamicArray<E> trimmed = new DynamicArray(Q.getSize());
 
 		int size_queue = Q.getSize();
@@ -61,44 +48,40 @@ public class DynamicArray<E extends Comparable<E>> {
 		return trimmed;
 	}
 
+	// ensure the array has enough space by creating a new one bigger
 	public DynamicArray<E> ensure() {
 		DynamicArray<E> scaled = new DynamicArray(this.length + this.alfa());
 
+		// copy the old array on the new one
 		for(int i = 0; i < this.length; i++)
 			scaled.set(i, (E)arr[i]);
 
 		return scaled;
 	}
 
+	// the costant that you add or subtract from the new one
 	protected int alfa(){
 		return this.length * 3/5;
 	}
 
+	// spare unused space of the array by creating a new one smaller
 	public DynamicArray<E> spare() {
 		Queue<Integer> Q = new Queue();
-		
+
+		// enqueue the values not null of the array
 		for(Integer i = 0; i < this.length; i++)
 			if (arr[i] != null)
 				Q.enqueue(i);
 
-		boolean check_size = ((this.length - this.alfa()) >= Q.getSize());		
+
+		boolean check_size = ((this.length - this.alfa()) >= Q.getSize());
 		DynamicArray<E> spared = new DynamicArray((check_size) ? this.length - this.alfa() : this.length);
 		int newLength = (check_size) ? Q.getSize() : this.length;
 
+		// insert them on the same order
 		for(int i = 0; i < newLength; i++)
 			spared.set(i, (E)arr[(check_size) ? Q.dequeue() : i]);
 
 		return spared;
-	}
-
-	@Override
-	public String toString() {
-		return Arrays.toString(arr);
-	}
-
-	// Function to converts the array in the actual array[] version
-	public E[] toPrimitive(){
-		final E[] primitive = (E[])arr;
-		return primitive;
 	}
 }
