9b9308e72711423aff62691f85a5375552680633
David Mack <mack.david.88@gmail.com>
2021-02-07 12:07:43 +0100

    :bulb: ArrayList DONE

--- movida/mackseverini/ArrayList.java
+++ movida/mackseverini/ArrayList.java
@@ -1,11 +1,13 @@
 package movida.mackseverini;
 
+// class needed because java.util.ArrayList didn't implements Comparable
 public class ArrayList<E extends Comparable<E>> extends java.util.ArrayList<E> implements Comparable<ArrayList<E>>{
   @Override
   public int compareTo(ArrayList<E> in){
     if (in.size() != this.size())
       return (in.size() < this.size()) ? 1 : -1;
 
+    // compare each node of the array in input, with this
     int row = 0, col = 0;
     for (E i : in){
       col = 0;
@@ -14,9 +16,11 @@ public class ArrayList<E extends Comparable<E>> extends java.util.ArrayList<E> i
       row += col;
     }
 
+    // even if they're not in the same order it will still return 0 
     return (row == 0) ? 0 : (row < 0) ? -1 : 1;
   }
 
+  // CLEAN:
   public void print(){
     int count = 0;
     for (E i : this){
